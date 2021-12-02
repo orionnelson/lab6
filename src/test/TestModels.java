642c5c3579dc5c56b91a926505b85dfddfa8192f
orionnelson <orionnelson@hotmail.com>
2021-12-02 17:04:05 -0500

    Set up Placeholder Test

---
+++ src/test/TestModels.java
@@ -0,0 +1,24 @@
+package test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ *	Class to test models and in game events
+ */
+public class TestModels{
+
+	/**
+	 *  Checks that the game player names are correctly converted 
+	 */
+	@Test
+	@DisplayName("Test Placeholder")
+	public void testPlaceholder(){
+		assertTrue(true);
+	}
+
+
+}
