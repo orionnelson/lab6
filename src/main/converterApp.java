31faf5d05e8e2e67323bf4c0aaeca0247cae1406
orionnelson <orionnelson@hotmail.com>
2021-12-02 16:55:34 -0500

    Set up Folder Structure

---
+++ src/main/converterApp.java
@@ -0,0 +1,17 @@
+package main;
+import java.util.*;
+
+/**
+ * Class to run soccer program.
+ */
+
+public class converterApp {
+	
+	/**
+	 * This is the main method of the soccer program.
+	 * @param args is command-line arguments.
+	 */
+	public static void main(String[] args) {
+		
+	}
+}
