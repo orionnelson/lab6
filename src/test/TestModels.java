74298520a3ba55462309a25f406d8c5fd3e33d47
denjcodes <d2j666@hotmail.com>
2021-12-05 17:28:39 -0500

    Update TestModels.java

--- src/test/TestModels.java
+++ src/test/TestModels.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import view.ProgPanel;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Objects;
 
 import javax.swing.JPanel;
 
@@ -23,87 +24,86 @@ import model.ValueToConvert;
  */
 public class TestModels {
 
-	
 	/**
 	 *  Checks conversion of CentArea to String Self and 
 	 */
 	@Test
 	@DisplayName("Test Centimeters Conversion to Feet")
 	public void testCentArea(){
-			ProgPanel p = new ProgPanel();
-			double areaM = 0.25;
-			double areaFt = 0.8202099737532809;
-			double areaCm = 25.0;
-			//Set subject for meter and feet area needed to do conversion test
-			String feetStr = areaFt + " ft";
-			String cmStr = Integer.toString((int)(areaCm));
-			String mStr = areaM + " m";
-			// All testing will be done as strings for absolute coverage since observer pattern makes testing difficult.
-			ValueToConvert v = new ValueToConvert(p);
-			v.save(cmStr);
-			//Carried Out By the User
-			
+		ProgPanel p = new ProgPanel();
+		double areaM = 0.25;
+		double areaFt = 0.8202099737532809;
+		double areaCm = 25.0;
+		//Set subject for meter and feet area needed to do conversion test
+		String feetStr = areaFt + " ft";
+		String cmStr = Integer.toString((int)(areaCm));
+		String mStr = areaM + " m";
+		// All testing will be done as strings for absolute coverage since observer pattern makes testing difficult.
+		ValueToConvert v = new ValueToConvert(p);
+		v.save(cmStr);
+		//Carried Out By the User
+
 		assertEquals(feetStr,p.getFeetConvArea().getText());
 	}
-	
-	
+
+
 	@Test
 	@DisplayName("Test Centimeters Conversion to Meters ")
 	public void testCentM(){
-			ProgPanel p = new ProgPanel();
-			double areaM = 0.25;
-			double areaFt = 0.8202099737532809;
-			double areaCm = 25.0;
-			//Set subject for meter and feet area needed to do conversion test
-			String feetStr = areaFt + " ft";
-			String cmStr = Integer.toString((int)(areaCm));
-			String mStr = areaM + " m";
-			// All testing will be done as strings for absolute coverage since observer pattern makes testing difficult.
-			ValueToConvert v = new ValueToConvert(p);
-			v.save(cmStr);
-			//Carried Out By the User
-			
+		ProgPanel p = new ProgPanel();
+		double areaM = 0.25;
+		double areaFt = 0.8202099737532809;
+		double areaCm = 25.0;
+		//Set subject for meter and feet area needed to do conversion test
+		String feetStr = areaFt + " ft";
+		String cmStr = Integer.toString((int)(areaCm));
+		String mStr = areaM + " m";
+		// All testing will be done as strings for absolute coverage since observer pattern makes testing difficult.
+		ValueToConvert v = new ValueToConvert(p);
+		v.save(cmStr);
+		//Carried Out By the User
+
 		assertEquals(mStr,p.getMeterConvArea().getText());
 	}
-	
-	
+
+
 	@Test
 	@DisplayName("Test Centimeters Update ")
 	public void testInput(){
-			ProgPanel p = new ProgPanel();
-			double areaM = 0.25;
-			double areaFt = 0.8202099737532809;
-			double areaCm = 25.0;
-			//Set subject for meter and feet area needed to do conversion test
-			String feetStr = areaFt + " ft";
-			String cmStr = Integer.toString((int)(areaCm));
-			String mStr = areaM + " m";
-			// All testing will be done as strings for absolute coverage since observer pattern makes testing difficult.
-			ValueToConvert v = new ValueToConvert(p);
-			v.save(cmStr);
-			//Carried Out By the User
-			
-			
+		ProgPanel p = new ProgPanel();
+		double areaM = 0.25;
+		double areaFt = 0.8202099737532809;
+		double areaCm = 25.0;
+		//Set subject for meter and feet area needed to do conversion test
+		String feetStr = areaFt + " ft";
+		String cmStr = Integer.toString((int)(areaCm));
+		String mStr = areaM + " m";
+		// All testing will be done as strings for absolute coverage since observer pattern makes testing difficult.
+		ValueToConvert v = new ValueToConvert(p);
+		v.save(cmStr);
+		//Carried Out By the User
+
+
 		assertEquals(cmStr,p.getCMConvArea().getText());
 	}
-	
-	
-	
+
+
+
 
 	@Test
 	public void testCentAreaStringToString(){
 		ProgPanel p = new ProgPanel();
 		double areaCm = 0.25;
-		
+
 		ValueToConvert v = new ValueToConvert(p);
 		v.save(Double.toString(areaCm);
 
 		//Test area Cm test is updated
 		assertEquals(Double.parseDouble(p.getCMConvArea().getText()), areaCm);
-		
+
 		//Test area Cm test is updated to "" if incorrect input
 		v.save("Garbage input");
-		assert(Objects.equals(p.getCMConvArea().getText(), ""));		
+		assert(Objects.equals(p.getCMConvArea().getText(), ""));
 	}
 
 }
