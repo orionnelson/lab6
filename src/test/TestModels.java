0a0e3f40b6239e56dbd960b0b1e2582671652fd5
denjcodes <d2j666@hotmail.com>
2021-12-05 16:46:27 -0500

    Testing to see changes
    Before uploading full testCases.java

--- src/test/TestModels.java
+++ src/test/TestModels.java
@@ -71,5 +71,14 @@ public class TestModels {
 		assertEquals(mStr,p.getMeterConvArea().getText());
 	}
 
+	@Test
+	public void testCentAreaStringToString(){
+		ProgPanel p = new ProgPanel();
+		double areaM = 0.25;
+		
+		p.getCMConvArea().setText(Double.toString(areaCm));
+
+		assert(Double.parseDouble(p.getCMConvArea().getText()) == areaCm);
+	}
 
 }
