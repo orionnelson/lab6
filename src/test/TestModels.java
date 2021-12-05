82103adf61beb4bd0dc764213da95d17731e666a
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2021-12-05 15:26:14 -0500

    Update TestModels.java

--- src/test/TestModels.java
+++ src/test/TestModels.java
@@ -6,6 +6,16 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import view.ProgPanel;
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+import model.CentimetersConversionArea;
+import model.FeetConversionArea;
+import model.MeterConversionArea;
+
 /**
  *	Class to test models and in game events
  */
@@ -15,8 +25,38 @@ public class TestModels{
 	 *  Checks that the game player names are correctly converted 
 	 */
 	@Test
-	@DisplayName("Test Placeholder")
-	public void testPlaceholder(){
+	@DisplayName("Test Centimeters Conversion to Area")
+	public void testCentArea(){
+			ProgPanel p = new ProgPanel();
+			double areaM = 0.25;
+			double areaFt = 0.8202099737532809;
+			double areaCm = 25.0;
+			//Set subject for meter and feet area needed to do conversion test
+			String feetStr = areaFt + " ft";
+			String cmStr = Integer.toString((int)(areaCm));
+			String mStr = areaM + " m";
+			
+			
+			// All testing will be done as strings for absolute coverage since observer pattern makes testing difficult.
+			FeetConversionArea feetConvArea = new FeetConversionArea("0 ft",15,25);
+			feetConvArea.setEditable(false);
+			feetConvArea.setBackground(Color.GREEN);
+			p.add(feetConvArea);
+			
+			MeterConversionArea meterConvArea = new MeterConversionArea("0 m",15,25);
+			meterConvArea.setEditable(false);
+			meterConvArea.setBackground(Color.ORANGE);
+			p.add(meterConvArea);
+		
+			CentimetersConversionArea cmConvArea = new CentimetersConversionArea(cmStr,15,25);
+			cmConvArea.setBackground(Color.YELLOW);
+			p.add(cmConvArea);
+			
+			
+			p.getCMConvArea().update();
+			System.out.println(p.getFeetConvArea().getText());
+			System.out.println(p.getMeterConvArea().getText());
+			
 		assertTrue(true);
 	}
 
