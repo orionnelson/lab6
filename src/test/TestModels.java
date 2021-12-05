5dea86e00277eb22a15cb67a06daf313493b9479
denjcodes <d2j666@hotmail.com>
2021-12-05 18:12:10 -0500

    Merge remote-tracking branch 'origin/testBuild-DJ' into testBuild-DJ

--- src/test/TestModels.java
+++ src/test/TestModels.java
@@ -7,8 +7,10 @@ import model.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+
 import view.ProgPanel;
 import java.awt.Color;
+<<<<<<< HEAD
 import java.awt.Graphics;
 import java.util.Collection;
 import java.util.List;
@@ -16,18 +18,34 @@ import java.util.Objects;
 
 import javax.swing.JPanel;
 
+=======
+
+import model.CentimetersConversionArea;
+import model.FeetConversionArea;
+import model.MeterConversionArea;
+
+>>>>>>> Update TestModels.java with test CMSubjectArea
 /**
  *	Class to test models and in game events
  */
-public class TestModels {
+public class TestModels{
 
 	/**
-	 *  Checks conversion of CentArea to String Self and
+	 *  Checks that the game player names are correctly converted
 	 */
+
+	/**
+<<<<<<< HEAD
+	 *  Checks conversion of CentArea to String Self and
+=======
+	 * Default Frame for testing
+>>>>>>> Update TestModels.java with test CMSubjectArea
+	 */
+	ProgPanel p = new ProgPanel();
+
 	@Test
-	@DisplayName("Test Centimeters Conversion to Feet")
+	@DisplayName("Test Centimeters Conversion to Area")
 	public void testCentArea(){
-			ProgPanel p = new ProgPanel();
 			double areaM = 0.25;
 			double areaFt = 0.8202099737532809;
 			double areaCm = 25.0;
@@ -35,14 +53,9 @@ public class TestModels {
 			String feetStr = areaFt + " ft";
 			String cmStr = Integer.toString((int)(areaCm));
 			String mStr = areaM + " m";
-			// All testing will be done as strings for absolute coverage since observer pattern makes testing difficult.
-			ValueToConvert v = new ValueToConvert(p);
-			v.save(cmStr);
-			
-			System.out.println(p.getFeetConvArea().getText());
-			System.out.println(p.getMeterConvArea().getText());
-			System.out.println(p.getCMConvArea().getText());
-			
+
+
+<<<<<<< HEAD
 		assertEquals(feetStr,p.getFeetConvArea().getText());
 	}
 
@@ -67,8 +80,53 @@ public class TestModels {
 			System.out.println(p.getCMConvArea().getText());
 
 		assertEquals(mStr,p.getMeterConvArea().getText());
+=======
+			// All testing will be done as strings for absolute coverage since observer pattern makes testing difficult.
+			FeetConversionArea feetConvArea = new FeetConversionArea("0 ft",15,25);
+			feetConvArea.setEditable(false);
+			feetConvArea.setBackground(Color.GREEN);
+			p.add(feetConvArea);
+
+			MeterConversionArea meterConvArea = new MeterConversionArea("0 m",15,25);
+			meterConvArea.setEditable(false);
+			meterConvArea.setBackground(Color.ORANGE);
+			p.add(meterConvArea);
+
+			CentimetersConversionArea cmConvArea = new CentimetersConversionArea(cmStr,15,25);
+			cmConvArea.setBackground(Color.YELLOW);
+			p.add(cmConvArea);
+
+
+
+			System.out.println(p.getFeetConvArea().getText());
+			System.out.println(p.getMeterConvArea().getText());
+
+			p.getCMConvArea().setText(Double.toString(areaCm));
+
+			assert(Double.parseDouble(p.getCMConvArea().getText()) == areaCm);
+
+
+		assertTrue(true);
+>>>>>>> Update TestModels.java with test CMSubjectArea
 	}
 
+	/**
+	 * Test Cent Area string is updated
+	 */
+	@Test
+	public void testCentAreaStringToString(){
+		double areaM = 0.25;
+		double areaFt = 0.8202099737532809;
+		double areaCm = 25.0;
+		//Set subject for meter and feet area needed to do conversion test
+		String feetStr = areaFt + " ft";
+		String cmStr = Integer.toString((int)(areaCm));
+		String mStr = areaM + " m";
+
+		p.getCMConvArea().setText(Double.toString(areaCm));
+
+		assert(Double.parseDouble(p.getCMConvArea().getText()) == areaCm);
+	}
 
 	@Test
 	@DisplayName("Test Centimeters Conversion to Null")
