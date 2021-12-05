e0546732341478f38f40b6302a93fdb4ab9e338c
denjcodes <d2j666@hotmail.com>
2021-12-05 17:46:05 -0500

    Merge remote-tracking branch 'origin/testBuild-DJ' into testBuild-DJ

--- src/test/TestModels.java
+++ src/test/TestModels.java
@@ -3,49 +3,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import model.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-
 import view.ProgPanel;
 import java.awt.Color;
-<<<<<<< HEAD
 import java.awt.Graphics;
-import java.util.Collection;
-import java.util.List;
 import java.util.Objects;
 
 import javax.swing.JPanel;
 
-=======
-
 import model.CentimetersConversionArea;
+import model.ConcreteSubject;
 import model.FeetConversionArea;
 import model.MeterConversionArea;
+import model.ValueToConvert;
 
->>>>>>> Update TestModels.java with test CMSubjectArea
 /**
  *	Class to test models and in game events
  */
-public class TestModels{
+public class TestModels {
 
 	/**
-	 *  Checks that the game player names are correctly converted
-	 */
-
-	/**
-<<<<<<< HEAD
 	 *  Checks conversion of CentArea to String Self and
-=======
-	 * Default Frame for testing
->>>>>>> Update TestModels.java with test CMSubjectArea
 	 */
-	ProgPanel p = new ProgPanel();
-
 	@Test
-	@DisplayName("Test Centimeters Conversion to Area")
+	@DisplayName("Test Centimeters Conversion to Feet")
 	public void testCentArea(){
+			ProgPanel p = new ProgPanel();
 			double areaM = 0.25;
 			double areaFt = 0.8202099737532809;
 			double areaCm = 25.0;
@@ -53,9 +38,14 @@ public class TestModels{
 			String feetStr = areaFt + " ft";
 			String cmStr = Integer.toString((int)(areaCm));
 			String mStr = areaM + " m";
-
-
-<<<<<<< HEAD
+			// All testing will be done as strings for absolute coverage since observer pattern makes testing difficult.
+			ValueToConvert v = new ValueToConvert(p);
+			v.save(cmStr);
+			
+			System.out.println(p.getFeetConvArea().getText());
+			System.out.println(p.getMeterConvArea().getText());
+			System.out.println(p.getCMConvArea().getText());
+			
 		assertEquals(feetStr,p.getFeetConvArea().getText());
 	}
 
@@ -80,56 +70,10 @@ public class TestModels{
 			System.out.println(p.getCMConvArea().getText());
 
 		assertEquals(mStr,p.getMeterConvArea().getText());
-=======
-			// All testing will be done as strings for absolute coverage since observer pattern makes testing difficult.
-			FeetConversionArea feetConvArea = new FeetConversionArea("0 ft",15,25);
-			feetConvArea.setEditable(false);
-			feetConvArea.setBackground(Color.GREEN);
-			p.add(feetConvArea);
-
-			MeterConversionArea meterConvArea = new MeterConversionArea("0 m",15,25);
-			meterConvArea.setEditable(false);
-			meterConvArea.setBackground(Color.ORANGE);
-			p.add(meterConvArea);
-
-			CentimetersConversionArea cmConvArea = new CentimetersConversionArea(cmStr,15,25);
-			cmConvArea.setBackground(Color.YELLOW);
-			p.add(cmConvArea);
-
-
-
-			System.out.println(p.getFeetConvArea().getText());
-			System.out.println(p.getMeterConvArea().getText());
-
-			p.getCMConvArea().setText(Double.toString(areaCm));
-
-			assert(Double.parseDouble(p.getCMConvArea().getText()) == areaCm);
-
-
-		assertTrue(true);
->>>>>>> Update TestModels.java with test CMSubjectArea
 	}
 
-	/**
-	 * Test Cent Area string is updated
-	 */
-	@Test
-	public void testCentAreaStringToString(){
-		double areaM = 0.25;
-		double areaFt = 0.8202099737532809;
-		double areaCm = 25.0;
-		//Set subject for meter and feet area needed to do conversion test
-		String feetStr = areaFt + " ft";
-		String cmStr = Integer.toString((int)(areaCm));
-		String mStr = areaM + " m";
-
-		p.getCMConvArea().setText(Double.toString(areaCm));
-
-		assert(Double.parseDouble(p.getCMConvArea().getText()) == areaCm);
-	}
 
 	@Test
-	@DisplayName("Test Centimeters Conversion to Null")
 	public void testCentAreaStringToString(){
 		ProgPanel p = new ProgPanel();
 		double areaCm = 0.25;
@@ -139,16 +83,6 @@ public class TestModels{
 		v.save("Garbage input");
 		assertEquals(p.getCMConvArea().getText(), "");
 
-	}
-
-	@Test
-	@DisplayName("Test Concrete Class Methods")
-	public void testConcreteSubject() {
-		ProgPanel p = new ProgPanel();
-		ValueToConvert v = new ValueToConvert(p);
-
-		v.addObserver(p.getCMConvArea());
-		v.removeObserver(p.getMeterConvArea());
 	}
 }
 
