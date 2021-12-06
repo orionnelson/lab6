64d0b14fa5089631223d490b3ccea1de1d047ae2
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2021-12-05 19:51:48 -0500

    Update TestModels.java

--- src/test/TestModels.java
+++ src/test/TestModels.java
@@ -72,35 +72,33 @@ public class TestModels{
 			// All testing will be done as strings for absolute coverage since observer pattern makes testing difficult.
 			ValueToConvert v = new ValueToConvert(p);
 			v.save(cmStr);
-
-			System.out.println(p.getFeetConvArea().getText());
-			System.out.println(p.getMeterConvArea().getText());
-			System.out.println(p.getCMConvArea().getText());
-
-			assertEquals(mStr,p.getMeterConvArea().getText());
-			// All testing will be done as strings for absolute coverage since observer pattern makes testing difficult.
-			FeetConversionArea feetConvArea = new FeetConversionArea("0 ft",15,25);
-			feetConvArea.setEditable(false);
-			feetConvArea.setBackground(Color.GREEN);
-			p.add(feetConvArea);
-
-			MeterConversionArea meterConvArea = new MeterConversionArea("0 m",15,25);
-			meterConvArea.setEditable(false);
-			meterConvArea.setBackground(Color.ORANGE);
-			p.add(meterConvArea);
-
-			CentimetersConversionArea cmConvArea = new CentimetersConversionArea(cmStr,15,25);
-			cmConvArea.setBackground(Color.YELLOW);
-			p.add(cmConvArea);
-
-			System.out.println(p.getFeetConvArea().getText());
-			System.out.println(p.getMeterConvArea().getText());
-
-			p.getCMConvArea().setText(Double.toString(areaCm));
-
-			assertEquals(Double.parseDouble(p.getCMConvArea().getText()),  areaCm);
+			//Carried Out By the User
+			
+		assertEquals(mStr,p.getMeterConvArea().getText());
 	}
-
+	
+	
+	@Test
+	@DisplayName("Test Centimeters Update ")
+	public void testInput(){
+			ProgPanel p = new ProgPanel();
+			double areaM = 0.25;
+			double areaFt = 0.8202099737532809;
+			double areaCm = 25.0;
+			//Set subject for meter and feet area needed to do conversion test
+			String feetStr = areaFt + " ft";
+			String cmStr = Integer.toString((int)(areaCm));
+			String mStr = areaM + " m";
+			// All testing will be done as strings for absolute coverage since observer pattern makes testing difficult.
+			ValueToConvert v = new ValueToConvert(p);
+			v.save(cmStr);
+			//Carried Out By the User
+			
+			
+		assertEquals(cmStr,p.getCMConvArea().getText());
+	}
+	
+	
 	/**
 	 * Test Cent Area string is updated
 	 */
